4a2a7036f3d2af3152dcf9a6a3d956a9b0f54078
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-01-29 18:01:48 -0500

    Add loop to print moves

--- hanoi.py
+++ hanoi.py
@@ -7,6 +7,8 @@ rods = {"A": list(range(NUMBER_OF_DISKS, 0, -1)), "B": [], "C": []}
 def move(n, source, auxiliary, target):
     # display starting configuration
     print(rods)
+    for move in range(number_of_moves):
+        print(move)
 
 
 # initiate call from source A to target C with auxiliary B
