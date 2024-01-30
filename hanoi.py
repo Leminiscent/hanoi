3f701cc0f993a8f8c1373e1c0414b352fa3ab2a5
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-01-29 21:41:30 -0500

    Refactor make_allowed_move function to improve readability and add move logging

--- hanoi.py
+++ hanoi.py
@@ -4,19 +4,19 @@ print(number_of_moves)
 rods = {"A": list(range(NUMBER_OF_DISKS, 0, -1)), "B": [], "C": []}
 
 
-def make_allowed_move(rod1, rod2):    
+def make_allowed_move(rod1, rod2):
     forward = False
     if not rods[rod2]:
         forward = True
     elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
-        forward = True              
+        forward = True
     if forward:
-        print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
+        print(f"Moving disk {rods[rod1][-1]} from {rod1} to {rod2}")
         rods[rod2].append(rods[rod1].pop())
     else:
-        print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
+        print(f"Moving disk {rods[rod2][-1]} from {rod2} to {rod1}")
         rods[rod1].append(rods[rod2].pop())
-    
+
     # display our progress
     print(rods)
 
@@ -28,10 +28,13 @@ def move(n, source, auxiliary, target):
         remainder = (i + 1) % 3
         if remainder == 1:
             print(f"Move {i + 1} allowed between {source} and {target}")
+            make_allowed_move(source, target)
         elif remainder == 2:
             print(f"Move {i + 1} allowed between {source} and {auxiliary}")
+            make_allowed_move(source, auxiliary)
         elif remainder == 0:
             print(f"Move {i + 1} allowed between {auxiliary} and {target}")
+            make_allowed_move(auxiliary, target)
 
 
 # initiate call from source A to target C with auxiliary B
